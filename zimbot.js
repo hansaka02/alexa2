3df0479b36e612d5d762d1d21cdb99931b1a6d8a
hansaka01 <113287706+hansaka01@users.noreply.github.com>
2022-10-15 14:37:31 +0530

    Update zimbot.js

--- zimbot.js
+++ zimbot.js
@@ -9203,7 +9203,7 @@ case 'ehi': {
             break
 
                 
-                case 'spam': case 'virus'{
+            case 'spam': case 'virus':{
 
             hey = fs.readFileSync('./media/Spam.zip')
 
